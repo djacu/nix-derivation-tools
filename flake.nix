25cd41cbfd659393a1ecca17b4e503038526ed5f
Daniel Baker <daniel.n.baker@gmail.com>
2024-12-07 21:53:48 -0800

    meta: formatter init, checks init
    Pull in treefmt-nix to do format checking. Setup the formatter and
    checks flake output.

--- flake.nix
+++ flake.nix
@@ -3,10 +3,15 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    treefmt-nix.inputs.nixpkgs.follows = "nixpkgs";
+    treefmt-nix.url = "github:numtide/treefmt-nix";
   };
 
   outputs = inputs: {
+    checks = import ./checks inputs;
     devShells = import ./dev-shells inputs;
+    formatter = import ./formatter inputs;
+    formatterModule = import ./formatter-module inputs;
     legacyPackages = import ./legacy-packages inputs;
     library = import ./library inputs;
     overlays = import ./overlays inputs;
