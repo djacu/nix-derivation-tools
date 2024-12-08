ad4ae9d7e96825cfb52c53ddbadb46db506c88c6
Daniel Baker <daniel.n.baker@gmail.com>
2024-12-07 21:14:47 -0800

    meta.packages: init
    Setting up the packages flake output.

--- packages/default.nix
+++ packages/default.nix
@@ -1,1 +1,21 @@
-inputs: { }
+inputs:
+let
+  inherit (inputs.nixpkgs)
+    lib
+    ;
+
+  inherit (lib.attrsets)
+    genAttrs
+    ;
+
+in
+genAttrs
+  [
+    "x86_64-linux"
+    "aarch64-linux"
+  ]
+  (system: {
+    inherit (inputs.self.legacyPackages.${system})
+      nix-derivation-parser
+      ;
+  })
