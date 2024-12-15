0e46cd229fe4058964800381181017800304a54e
Daniel Baker <daniel.n.baker@gmail.com>
2024-12-15 11:36:19 -0800

    meta: add pre-commit hooks via git-hooks.nix
    Adds `git-hooks.nix` as a flake input. Adds `flake-compat` and
    `gitignore.nix` as flake inputs as well so we can pin all the inputs of
    `git-hooks.nix`. Set all the recently added inputs to follow inputs we
    specify.
    
    Added `pre-commit` checks and used the `treefmt` option with the
    `treefmt` package defined by `treefmt-nix`. Added packages and a shell
    hook to the `bootstrap` devShell so it will install pre-commit
    configuration.
    
    Added `.pre-commit-config.yaml` to the .gitignore as instructed.

--- flake.nix
+++ flake.nix
@@ -2,7 +2,16 @@
   description = "nix-derivation-parser";
 
   inputs = {
+    flake-compat.flake = false;
+    flake-compat.url = "github:edolstra/flake-compat";
+    gitignore.inputs.nixpkgs.follows = "nixpkgs";
+    gitignore.url = "github:hercules-ci/gitignore.nix";
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    pre-commit-hooks.inputs.flake-compat.follows = "flake-compat";
+    pre-commit-hooks.inputs.gitignore.follows = "gitignore";
+    pre-commit-hooks.inputs.nixpkgs-stable.follows = "nixpkgs";
+    pre-commit-hooks.inputs.nixpkgs.follows = "nixpkgs";
+    pre-commit-hooks.url = "github:cachix/git-hooks.nix";
     treefmt-nix.inputs.nixpkgs.follows = "nixpkgs";
     treefmt-nix.url = "github:numtide/treefmt-nix";
   };
