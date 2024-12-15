b68e05002a35d72bc36da0950a5bd7ed06b7ce9c
Daniel Baker <daniel.n.baker@gmail.com>
2024-12-14 19:38:30 -0800

    treewide: format all
    Run `nix fmt`. genemichaels was previously enabled and formats
    differently than rustfmt.

--- packages/nix-derivation-parser/src/strings/types.rs
+++ packages/nix-derivation-parser/src/strings/types.rs
@@ -1,6 +1,6 @@
-/// A string fragment contains a fragment of a string being parsed: either
-/// a non-empty Literal (a series of non-escaped characters), a single
-/// parsed escaped character, or a block of escaped whitespace.
+/// A string fragment contains a fragment of a string being parsed: either a
+/// non-empty Literal (a series of non-escaped characters), a single parsed escaped
+/// character, or a block of escaped whitespace.
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 pub enum StringFragment<'a> {
     Literal(&'a str),
