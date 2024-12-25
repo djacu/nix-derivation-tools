cca59586a71e53920c6df61797b8ca056997e046
Daniel Baker <daniel.n.baker@gmail.com>
2024-12-24 21:13:06 -0800

    nix-derivation-parser: handle exhaustive attributes

--- packages/nix-derivation-parser/src/strings/types.rs
+++ packages/nix-derivation-parser/src/strings/types.rs
@@ -2,6 +2,7 @@
 /// non-empty Literal (a series of non-escaped characters), a single parsed escaped
 /// character, or a block of escaped whitespace.
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[non_exhaustive]
 pub enum StringFragment<'input> {
     Literal(&'input str),
     EscapedChar(char),
