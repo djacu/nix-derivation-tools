fcfca0c5b2e529380e6b3a6b7aa3e6f28ebd4614
Daniel Baker <daniel.n.baker@gmail.com>
2024-12-25 18:19:52 -0800

    nix_derivation_parser: use parse in main
    Using parse ensures we hit the end of the file.

--- packages/nix-derivation-parser/src/main.rs
+++ packages/nix-derivation-parser/src/main.rs
@@ -7,7 +7,7 @@ fn main() {
         fs::read_to_string(
             "./src/vlv5v250k5daq2dnhj3bzn7p5dnsrg2f-nixos-system-massflash-24.05.20241009.d51c286.drv",
         ).expect("Invalid file");
-    match parse_derivation::<()>.parse_next(&mut input.as_str()) {
+    match parse_derivation::<()>.parse(&mut input.as_str()) {
         Ok(parsed) => {
             println!("Parsed: {parsed:#?}");
         },
